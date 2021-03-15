3b7fab06d41d44603e402d29ac83c29aa2bc7d00
frankhn <harfrank2@gmail.com>
2021-03-15 08:28:02 +0200

    using hashMap

---
+++ HashMap/HashMapIndex.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class HashMapIndex {
+    public static void main(String[] args) {
+        // Create a HashMap object called capitalCities
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+        // Add keys and values (Country and cities)
+
+        capitalCities.put("Rwanda", "Kigali");
+        capitalCities.put("Tanzania", "Dodoma");
+
+        System.out.println(capitalCities);
+    }
+}
+
+// A HashSet is a collection of items where every item is unique, and it is
+// found in the java.util package:
