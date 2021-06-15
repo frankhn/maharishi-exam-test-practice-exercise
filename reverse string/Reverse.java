ec60f356ee9419fe585be08966ff14a58f3c0c1c
frankhn <harfrank2@gmail.com>
2021-06-15 16:59:08 +0200

    adds a reverce a number

--- reverse string/Reverse.java
+++ reverse string/Reverse.java
@@ -3,17 +3,36 @@
  */
 public class Reverse {
 
-    public static String reverseString(String string) {
+    public static String reverseString(String str) {
 
         String reverse = "";
-        for (int i = string.length() -1; i >= 0; i--) {
-            reverse += string.charAt(i);
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.println(str.charAt(i));
+            reverse += str.charAt(i);
         }
         return reverse;
     }
 
+    static Integer reverseNumber(Integer num) {
+    int reversed = 0;
+
+    // run loop until num becomes 0
+    while (num != 0) {
+        // get last digit from num
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        System.out.println("revesed: " + reversed + "digit: " + digit + "num: " + (num /= 10));
+
+        // remove the last digit from num
+        num /= 10;
+    }
+    return reversed;
+    }
+
     public static void main(String[] args) {
-        String string = "Franklin";
-        System.out.println(reverseString(string));
+        // String string = "Franklin";
+        // System.out.println(reverseString(string));
+        int num = 12345;
+        System.out.println(reverseNumber(num));
     }
 }
